6001e0bef3c1d5690e637ce0c41c8669193b62f7
Robert Nagy <ronagy@icloud.com>
2024-07-11 09:33:11 +0200

    chore: add benchmark

---
+++ benchmarks/rocks-vs-map.mjs
@@ -0,0 +1,68 @@
+import { bench, run } from 'mitata'
+import { RocksLevel } from '../index.js'
+import { LRUCache } from 'lru-cache'
+
+const values = []
+for (let x = 0; x < 1e3; x++) {
+  values.push(Buffer.from(Math.random().toString(36).repeat(4)))
+}
+
+const db = new RocksLevel('./tmp', {
+  keyEncoding: 'buffer',
+  valueEncoding: 'buffer',
+  parallelism: 4,
+  pipelinedWrite: false,
+  // unorderedWrite: true,
+  columns: {
+    default: {
+      cacheSize: 128e6,
+      memtableMemoryBudget: 128e6,
+      compaction: 'level',
+      // optimize: 'point-lookup',
+    },
+  },
+})
+await db.open()
+
+const map = new Map()
+const lru = new LRUCache({ max: 10e3 })
+
+for (const value of values) {
+  await db.put(value, value)
+  map.set(value.toString(), value.toString())
+  lru.set(value.toString(), value)
+}
+
+let x = 0
+
+const getOpts = {
+  keyEncoding: 'buffer',
+  valueEncoding: 'buffer',
+  fillCache: true,
+}
+
+db._getMany(values, getOpts).length
+
+bench('rocks async', async () => {
+  x += (await db._getMany(values, getOpts)).length
+})
+
+bench('rocks sync', async () => {
+  x += db._getManySync(values, getOpts).length
+})
+
+bench('map', () => {
+  for (const value of values) {
+    x += map.get(value.toString()).length
+  }
+})
+
+bench('lru', () => {
+  for (const value of values) {
+    x += lru.get(value.toString()).length
+  }
+})
+
+await run()
+
+console.log(x)
