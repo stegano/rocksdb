7c086e65044771d76177cd9ec9a021f91db5c51a
Robert Nagy <ronagy@icloud.com>
2024-07-25 17:18:30 +0200

    encoding

--- index.js
+++ index.js
@@ -162,16 +162,18 @@ class RocksLevel extends AbstractLevel {
           const rows = []
           let offset = 0
           const sizes32 = new Int32Array(sizes.buffer, sizes.byteOffset, sizes.byteLength / 4)
-          for (const size of sizes32) {
+          for (let n = 0; n < sizes32.length; n++) {
+            const size = sizes32[n]
+            const encoding = valueEncoding
             if (size < 0) {
               rows.push(undefined)
             } else {
-              if (!valueEncoding || valueEncoding === 'buffer') {
+              if (!encoding || encoding === 'buffer') {
                 rows.push(data.subarray(offset, offset + size))
-              } else if (valueEncoding === 'slice') {
+              } else if (encoding === 'slice') {
                 rows.push({ buffer: data, byteOffset: offset, byteLength: size })
               } else {
-                rows.push(data.toString(valueEncoding, offset, offset + size))
+                rows.push(data.toString(encoding, offset, offset + size))
               }
               offset += size
               if (offset & 0x7) {
