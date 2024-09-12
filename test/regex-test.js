15cab08654b3f6ddd95477165d164b0f49da34a3
Robert Nagy <ronagy@icloud.com>
2024-09-12 14:32:57 +0200

    fix: add test

--- test/regex-test.js
+++ test/regex-test.js
@@ -7,5 +7,6 @@ test('regex.test', (t) => {
   t.ok(new Regex('.*').test(Buffer.from('asd:user')))
   t.ok(new Regex(':user').test(Buffer.from('asd:user')))
   t.ok(!new Regex(':user$').test(Buffer.from('asd:user123')))
+  t.ok(!new Regex('^(?:({.*}):)?(render\.(stats|methods|result|description|query|args|status|opts))(?:[?].*)?$').test(Buffer.from('asd:user123')))
   t.end()
 })
