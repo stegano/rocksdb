5a35bbef0d4574bb6e98df4cf0b1c72420bb1d5a
Robert Nagy <ronagy@icloud.com>
2024-09-28 20:09:00 +0200

    perf: no-copy read

--- binding.cc
+++ binding.cc
@@ -1133,7 +1133,7 @@ NAPI_METHOD(db_get_many) {
             NAPI_STATUS_RETURN(napi_get_null(env, &row));
           } else {
             ROCKS_STATUS_RETURN_NAPI(state.statuses[n]);
-            NAPI_STATUS_RETURN(Convert(env, &state.values[n], valueEncoding, row));
+            NAPI_STATUS_RETURN(Convert(env, std::move(state.values[n]), valueEncoding, row));
           }
           NAPI_STATUS_RETURN(napi_set_element(env, argv[1], n, row));
         }
